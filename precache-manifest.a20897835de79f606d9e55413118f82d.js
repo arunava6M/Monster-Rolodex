ed938a7255a0078104e30ac912567121d511999e
ArunavaM <arunava6mondal@gmail.com>
2019-11-12 19:48:26 +0530

    Updates

---
+++ precache-manifest.a20897835de79f606d9e55413118f82d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca204fde953df532f263d55f6cb550b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e79a812fa8a4e9f6a11e",
+    "url": "/monster-rolodex/static/css/main.6cf4fa7f.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "/monster-rolodex/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "e79a812fa8a4e9f6a11e",
+    "url": "/monster-rolodex/static/js/main.1bec9cb5.chunk.js"
+  },
+  {
+    "revision": "aff08a4d83f58743dcb3",
+    "url": "/monster-rolodex/static/js/runtime-main.400ce9bc.js"
+  }
+]);
